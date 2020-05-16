51b63691c43ca23e609b4bcdc5282c4df88fea2d
ianmcderp <Dh5!20$4>
2020-05-16 13:14:43 +0200

    Added Swagger support

--- src/main/java/rocks/riedl/adqc/controller/HelloGradleController.java
+++ src/main/java/rocks/riedl/adqc/controller/HelloGradleController.java
@@ -1,13 +1,16 @@
 package rocks.riedl.adqc.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/")
+@RequestMapping("/")
 public class HelloGradleController {
 
   @GetMapping
   public String helloGradle() {
     return "Hello Gradle!";
   }
+
 }
